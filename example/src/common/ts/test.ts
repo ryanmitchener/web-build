1606a96d638943ab1c69990040190e468ae2b69f
Ryan Mitchener <RyanMitchener@gmail.com>
2017-07-08 01:30:16 -0400

    Added testing for TypeScript and Rollup

--- example/src/common/ts/test.ts
+++ example/src/common/ts/test.ts
@@ -1,4 +1,13 @@
+import Test2 from "./test2"
+
 class Test {
     testVar = 1;
     testVar2 = 2;
+
+    main() {
+        var t = new Test2();
+        t.hello();
+    }
 }
+
+new Test().main();
--- example/src/common/ts/test2.ts
+++ example/src/common/ts/test2.ts
@@ -1,5 +1,13 @@
-class Test2 {
+export default class Test2 {
     testVar = 1;
     testVar2 = 2;
     testVar3 = 3;
+
+    constructor() {
+        this.testVar = 2;
+    }
+
+    hello() {
+        console.log("Hello");
+    }
 }
